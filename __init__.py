15ba5784546ccf55a1b72983a6fa9031e00e3f30
Rafael Tomazzi <rafaeltomazzi@gmail.com>
2025-10-22 16:31:21 -0400

    -adicionado feature 04 atualizar preco de algum produto
    -adicionado condicao de checagem de estoque negativo na opcao 3

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ count = 0
 ligado = True
 ligadoVezes = 0
 
+caixa = 0.0
 
 estoque = {
     "maca": {"quantidade": 10, "preco": 2.5},
@@ -9,37 +10,44 @@ estoque = {
     "laranja": {"quantidade": 8, "preco": 4.0},
 }
 
+def getQuantFruta(fruta):
+    for k,v in estoque.items():
+        if fruta == k:
+            print(f"o estoque de {fruta} atual :")
+            return v['quantidade'] 
+
+
+def getPrecoFruta(fruta):
+    for k,v in estoque.items():
+        if fruta == k:
+            print(f"a {fruta} custa R$ {v['preco']}")
+            return v['preco'] 
+
 
 while ligado:
-    ligadoVezes += 1
-    print(f"inicio contagem: {ligadoVezes}\n")
-
-    def getQuantFruta(fruta):
-        for k,v in estoque.items():
-            if fruta == k:
-                print(f"o estoque de {fruta} atual :")
-                return v['quantidade'] 
+    #ligadoVezes += 1
+    #print(f"inicio contagem: {ligadoVezes}\n")
 
 
     opcao_str = input("digite a opção:\n"
-        "1 - quantidade \n"
-        "2 - adicionar produto novo \n"
+        "1 - Checar estoque \n"
+        "2 - Adicionar produto novo \n"
         "3 - Vender produto \n"
-        "4 - atualizar preço de algum produto \n"
-        "5 - sair \n"
+        "4 - Atualizar preço de algum produto \n"
+        "5 - Sair \n"
     )
 
-    print(f"Você selecionou a opção {opcao_str}")
+    print(f"Você selecionou a opção {opcao_str}\n")
     opcao_int = int(opcao_str)
 
 
 
 
     if opcao_int == 1:
-        print("As quantidades são:1")
+        print("As quantidades são: \n")
         for fruta, dado in estoque.items():
             print(f"a {fruta} tem {dado['quantidade']} unidades e custa R$ {dado['preco']}")
-
+        
 
     if opcao_int == 2:
         add_item = input("Qual fruta você deseja adicionar?\n")
@@ -54,16 +62,41 @@ while ligado:
         for k,v in estoque.items():
             count += 1
             print(f"{count} - {k}")
-            
+        
+        
         produtoVenda = input(f"Qual produto você quer vender? \n")
         for k,v in estoque.items():
             if produtoVenda == k:
+                print(f"Quantidade atual de {k}: {v['quantidade']}")
                 quantidadeVenda = int(input(f"qual a quantidade de {produtoVenda} que voce quer vender?\n"))
-                v['quantidade'] -= quantidadeVenda
+                if   quantidadeVenda > v["quantidade"]:
+                    print("Nao temos estoque")
+                    continue
+                else:
+                    v['quantidade'] -= quantidadeVenda
+                
                 print(getQuantFruta(produtoVenda))
             else:
                 continue
 
+
+    if opcao_int == 4:
+        print("Qual fruta voce quer atualizar o preco?")
+        for k,v in estoque.items():
+            print(k)
+        
+        frutaUpdatePreco = input("")
+        for k,v in estoque.items():
+            if frutaUpdatePreco == k:
+                print(f"Preco atual: {v['preco']}")
+                novoPreco = float(input(f"Qual o novo preco da fruta {k}? "))
+                v['preco'] = novoPreco
+            else:
+                continue
+
+        
+
     if opcao_int == 5:
         print("Voce saiu do programa")
         ligado = False
+    count = 0
